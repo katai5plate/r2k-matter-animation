67f654497bef4a5012d5b7ebbfbc7d0fdc2dd882
katai5plate <katai5plate.sub@gmail.com>
2018-11-21 08:47:41 +0900

    simulation!

--- src/app.js
+++ src/app.js
@@ -1,28 +1,59 @@
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
-
-// create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
+const Engine = Matter.Engine
+const Render = Matter.Render
+const World = Matter.World
+const Bodies = Matter.Bodies
+const engine = Engine.create({
+  timing:{
+    timeScale: 0.25
+  }
+})
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 320,
+    height: 240
+  }
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+const fixPos = (x, y, w, h) => [x + (w / 2), y + (h / 2), w, h]
+const toRad = (Math.PI * 2) / 360
+const $wall = {
+  isStatic: true
+};
+const $yoko = {
+  angle: 90 * toRad
+};
+const _rect = [16, 32];
+const _cube = [32, 32];
 
-// add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+const genRect = (x, y, angle) => Bodies.rectangle(...fixPos(x, y, 16, 32), {
+  angle: angle * toRad
+});
+const genCube = (x, y, angle) => Bodies.rectangle(...fixPos(x, y, 32, 32), {
+  angle: angle * toRad
+});
 
-// run the engine
+const walls = [
+  Bodies.rectangle(...fixPos(0, 0, 320, 40), $wall),
+  Bodies.rectangle(...fixPos(0, 200, 320, 40), $wall),
+  Bodies.rectangle(...fixPos(-10, 0, 10, 240), $wall),
+  Bodies.rectangle(...fixPos(320, 0, 10, 240), $wall),
+];
+const items = [
+  genRect(112, 160, 0),
+  genRect(112, 144, 90),
+  genRect(128, 112, 0),
+  genRect(112, 96, 90),
+  genRect(128, 64, 0),
+  genRect(112, 48, 90),
+];
+
+const objects = [
+  ...walls,
+  ...items,
+]
+
+World.add(engine.world, objects);
 Engine.run(engine);
-
-// run the renderer
 Render.run(render);
